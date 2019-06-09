f3260adf2bdc99b59b998a56301f5a0bdf3d5f9d
Jose Tejada <jtejadavilca@gmail.com>
2019-06-08 22:09:00 -0500

    proyecto inicial en SpringBoot como cliente hystrix para JavaExpert de CJAVA

---
+++ src/main/java/pe/com/pruebas/clienthystrixendpointjtv/ClientHystrixEndpointJtvApplication.java
@@ -0,0 +1,13 @@
+package pe.com.pruebas.clienthystrixendpointjtv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientHystrixEndpointJtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientHystrixEndpointJtvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/pruebas/clienthystrixendpointjtv/ClientHystrixEndpointJtvApplicationTests.java
@@ -0,0 +1,16 @@
+package pe.com.pruebas.clienthystrixendpointjtv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientHystrixEndpointJtvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
