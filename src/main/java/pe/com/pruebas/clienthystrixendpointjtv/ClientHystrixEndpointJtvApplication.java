439b40a8e23d823a281635f882b2556877af5959
Jose Tejada <jtejadavilca@gmail.com>
2019-06-09 01:06:35 -0500

    Se agrega notacion de eureka-client y de hystrix para el circuit breaker

--- src/main/java/pe/com/pruebas/clienthystrixendpointjtv/ClientHystrixEndpointJtvApplication.java
+++ src/main/java/pe/com/pruebas/clienthystrixendpointjtv/ClientHystrixEndpointJtvApplication.java
@@ -2,12 +2,32 @@ package pe.com.pruebas.clienthystrixendpointjtv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 @SpringBootApplication
 public class ClientHystrixEndpointJtvApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientHystrixEndpointJtvApplication.class, args);
+	}
+	
+	
+	@Configuration
+	class Config{
+		@LoadBalanced
+		@Bean
+		public RestTemplate restTemplate() {
+			return new RestTemplate();
+		}
 	}
 
 }
